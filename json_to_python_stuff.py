6621c1bad35b00e6a1fc874f25f4618f1f36a9cf
Daniel Tan <danieljktan@yahoo.com>
2018-06-11 16:56:33 -0700

    Java -> Python started

--- json_to_python_stuff.py
+++ json_to_python_stuff.py
@@ -27,9 +27,38 @@ def check_sparse_matrix(sparse_matrix, x, y):
     return x in sparse_matrix and y in sparse_matrix[x]
 
 
+#convert Java 448-480 lines to python....
+#one-to-one conversion might not be possible...depending on how it might be implemented...
+#types == set
+def look_up_type(types, reaction_matrix):
+    results = set()
+    for t1 in types:
+        for t2 in types:
+            results.update(look_up_a_b(t1, t2, reaction_matrix))
 
-sparse_matrix = create_sparse_matrix(r"C:\Users\Daniel Tan\Documents\ChemType\resources\epa.json") #id's and list
-print(check_sparse_matrix(sparse_matrix, 3, 1))
-print(sparse_matrix[100][20])
+    return results
+
+
+def look_up_a_b(a, b, reaction_matrix):
+    if a > b:
+        a, b = b, a
+
+    if a in reaction_matrix and b in reaction_matrix[a]:
+        return map(lambda x: get_type_from_id(x), reaction_matrix[a][b])
+    else:
+        return {get_type_from_id(a), get_type_from_id(b)}
+
+
+def get_type_from_id(x):
+    return 100
+
+
+
+print(look_up_type({1, 2, 3}, {1: {2: {1,2,3,4,5}}}))
+
+
+#sparse_matrix = create_sparse_matrix(r"C:\Users\Daniel Tan\Documents\ChemType\resources\epa.json") #id's and list
+#print(check_sparse_matrix(sparse_matrix, 3, 1))
+#print(sparse_matrix[100][20])
 
 
