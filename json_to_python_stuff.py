afb2deb1394c0124ebc4c20b4ea7f960f5994a8d
Daniel Tan <danieljktan@yahoo.com>
2018-06-11 15:56:17 -0700

    Corrected the sparse matrix which was incorrect

--- json_to_python_stuff.py
+++ json_to_python_stuff.py
@@ -1,7 +1,7 @@
 import json
 
 
-#creates a sparse_matrix dictionary w/ key = id, value = reaction_list (id: xxx, outcome: yyy)
+#creates a sparse_matrix dictionary w/ (key = id), value = dictionary of ( key = id, value = outcome)
 def create_sparse_matrix(file_name):
     file_read = open(file_name)
     json_dict = json.loads(file_read.read())['chemicalgroups']['group']
@@ -10,12 +10,12 @@ def create_sparse_matrix(file_name):
     for json_item in json_dict:  #json_dict is NOT a dict.... the .items() function doesn't work!!!
         if 'reactivegroups' in json_item and json_item['reactivegroups'] != None and len(json_item['reactivegroups']) != 0:
             key   = int(json_item['id'])
-            value = []
+            value = {}
             
             #horrible naming convention...find a better one....
             for j in json_item['reactivegroups']['reaction']:
                 if ('id' in j) and ('outcome' in j): #and (j['outcome'] == 'N'):
-                    value.append(j)
+                    value[int(j['id'])] = j['outcome']
 
             sparse_matrix[key] = value
 
@@ -23,15 +23,13 @@ def create_sparse_matrix(file_name):
     return sparse_matrix
 
 
+def check_sparse_matrix(sparse_matrix, x, y):
+    return x in sparse_matrix and y in sparse_matrix[x]
+
+
+
 sparse_matrix = create_sparse_matrix(r"C:\Users\Daniel Tan\Documents\ChemType\resources\epa.json") #id's and list
-for k, v in sparse_matrix.items():
-    print('id: ', k)
-    print('reactive groups: ', v)
-    print('\n\n\n\n\n')
-
-    #for vv in v:
-    #    print(vv['outcome'])
-
-    #print(len(v), v['id'], v['outcome'])
+print(check_sparse_matrix(sparse_matrix, 3, 1))
+print(sparse_matrix[100][20])
 
 
