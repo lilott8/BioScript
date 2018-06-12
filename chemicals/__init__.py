02e103e1b45651bf314b33cc7c8a26cf7c1219ec
Daniel Tan <danieljktan@yahoo.com>
2018-06-12 15:35:02 -0700

    Moved around more stuff

--- chemicals/__init__.py
+++ chemicals/__init__.py
@@ -1,5 +1,5 @@
 import json
-
+#from enums.problem import Problem
 
 #creates a sparse_matrix dictionary w/ (key = id), value = dictionary of ( key = id, value = outcome)
 def create_sparse_matrix(file_name):
@@ -30,27 +30,24 @@ def check_sparse_matrix(sparse_matrix, x, y):
 #convert Java 448-480 lines to python....
 #one-to-one conversion might not be possible...depending on how it might be implemented...
 #types == set
-def look_up_type(types, reaction_matrix):
-    results = set()
-    for t1 in types:
-        for t2 in types:
-            results.update(look_up_a_b(t1, t2, reaction_matrix))
+#def look_up_type(types, reaction_matrix):
+#    results = set()
+#    for t1 in types:
+#        for t2 in types:
+#            results.update(look_up_a_b(t1, t2, reaction_matrix))
 
-    return results
-
-
-def look_up_a_b(a, b, reaction_matrix):
-    if a > b:
-        a, b = b, a
-
-    if a in reaction_matrix and b in reaction_matrix[a]:
-        return map(lambda x: get_type_from_id(x), reaction_matrix[a][b])
-    else:
-        return {get_type_from_id(a), get_type_from_id(b)}
+#    return results
 
 
+#def look_up_a_b(a, b, reaction_matrix):
+#    if a > b:
+#        a, b = b, a
 
-
+#    if a in reaction_matrix and b in reaction_matrix[a]:
+#        return map(lambda x: Problem.get_type_from_id(x), reaction_matrix[a][b])
+#    else:
+#        return {Problem.get_type_from_id(a), Problem.get_type_from_id(b)}
+#look_up_type({1, 2}, {1:{2:{3, 4, 5}}})
 
 
 
--- enums/problem.py
+++ enums/problem.py
@@ -5,3 +5,60 @@ class Problem(Enum):
     STORAGE = 100
     DISPOSAL = 10
     MIX = 0
+
+#python enum is called by: x = Consequence(Consequence.H)
+class Consequence(Enum):
+    H   = 0
+    F   = 1
+    G   = 2
+    GT  = 3
+    GF  = 4
+    E   = 5
+    P   = 6
+    S   = 7
+    U   = 8
+    WRS = 9
+    N   = 10
+    C   = 11
+    SR  = 12
+
+    def __init__(self, Type):
+        self.value = Type
+
+    #toString() from Java, if you will...
+    def __str__(self):
+        if self.value == Consequence.H:
+            return "Head Generation"
+        elif self.value == Consequence.F:
+            return "Fire"
+        elif self.value == Consequence.G:
+            return "Innocuous and non-Flammable Gas generation"
+        elif self.value == Consequence.GT:
+            return "Toxic Gas Formation"
+        elif self.value == Consequence.GF:
+            return "Flammable Gas Formation"
+        elif self.value == Consequence.E:
+            return "Explosion"
+        elif self.value == Consequence.P:
+            return "Violent Polymerization"
+        elif self.value == Consequence.S:
+            return "Solubilization of Toxic Substance"
+        elif self.value == Consequence.U:
+            return "Unknown"
+        elif self.value == Consequence.WRS:
+            return "Water Reactive Substance"
+        elif self.value == Consequence.N:
+            return "Incompatible"
+        elif self.value == Consequence.C:
+            return "Caution"
+        elif self.value == Consequence.SR:
+            return "Self Reactive"
+        else:
+            return "????"
+
+
+#dummy function for now...
+def get_type_from_id(x):
+    return x
+
+
