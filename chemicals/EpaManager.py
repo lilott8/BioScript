ed32220321adb3e5dad0334441f0262a23484f3d
Daniel Tan <danieljktan@yahoo.com>
2018-06-15 12:21:24 -0700

    Refactored to better follow Google Style Guide

--- chemicals/EpaManager.py
+++ chemicals/EpaManager.py
@@ -3,9 +3,11 @@ import json
 class EpaManager(object):
     #creates a sparse_matrix dictionary w/ (key = id), value = dictionary of ( key = id, value = outcome)
     def create_sparse_matrix(file_name):
-        file_read = open(file_name)
-        json_dict = json.loads(file_read.read())['chemicalgroups']['group']
+        json_dict = []
         sparse_matrix = {}
+
+        with open(file_name) as file_read:
+            json_dict = json.loads(file_read.read())['chemicalgroups']['group']
 
         for json_item in json_dict:  #json_dict is NOT a dict.... the .items() function doesn't work!!!
             if 'reactivegroups' in json_item and json_item['reactivegroups'] != None and len(json_item['reactivegroups']) != 0:
