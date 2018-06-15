f9f18881c407c2f49e5b630624bce24b3dba58b8
Daniel Tan <danieljktan@yahoo.com>
2018-06-15 12:04:21 -0700

    clean up some files

--- chemicals/__init__.py
+++ chemicals/__init__.py
@@ -1,26 +1,3 @@
-#convert Java 448-480 lines to python....
-#one-to-one conversion might not be possible...depending on how it might be implemented...
-#types == set
-#def look_up_type(types, reaction_matrix):
-#    results = set()
-#    for t1 in types:
-#        for t2 in types:
-#            results.update(look_up_a_b(t1, t2, reaction_matrix))
-
-#    return results
-
-
-#def look_up_a_b(a, b, reaction_matrix):
-#    if a > b:
-#        a, b = b, a
-
-#    if a in reaction_matrix and b in reaction_matrix[a]:
-#        return map(lambda x: Problem.get_type_from_id(x), reaction_matrix[a][b])
-#    else:
-#        return {Problem.get_type_from_id(a), Problem.get_type_from_id(b)}
-#look_up_type({1, 2}, {1:{2:{3, 4, 5}}})
-
-
 
 
 
--- enums/Consequence.py
+++ enums/Consequence.py
@@ -2,54 +2,40 @@ from enum import Enum
 
 #python enum is called by: x = Consequence(Consequence.H)
 class Consequence(Enum):
-    H   = 0
-    F   = 1
-    G   = 2
-    GT  = 3
-    GF  = 4
-    E   = 5
-    P   = 6
-    S   = 7
-    U   = 8
-    WRS = 9
-    N   = 10
-    C   = 11
-    SR  = 12
-
-    '''def __str__(self):
-        if self.value == Consequence.H:
-            return "Head Generation"
-        elif self.value == Consequence.F:
-            return "Fire"
-        elif self.value == Consequence.G:
-            return "Innocuous and non-Flammable Gas generation"
-        elif self.value == Consequence.GT:
-            return "Toxic Gas Formation"
-        elif self.value == Consequence.GF:
-            return "Flammable Gas Formation"
-        elif self.value == Consequence.E:
-            return "Explosion"
-        elif self.value == Consequence.P:
-            return "Violent Polymerization"
-        elif self.value == Consequence.S:
-            return "Solubilization of Toxic Substance"
-        elif self.value == Consequence.U:
-            return "Unknown"
-        elif self.value == Consequence.WRS:
-            return "Water Reactive Substance"
-        elif self.value == Consequence.N:
-            return "Incompatible"
-        elif self.value == Consequence.C:
-            return "Caution"
-        elif self.value == Consequence.SR:
-            return "Self Reactive"
-        else:
-            return "????"'''
+    Head_Generation   = 0
+    Fire   = 1
+    Innocuous_and_Non_Flammable_Gas_Generation   = 2
+    Toxic_Gas_Formation  = 3
+    Flammable_Gas_Formation  = 4
+    Explosion   = 5
+    Violent_Polymerization   = 6
+    Solubilization_of_Toxic_Substance   = 7
+    Unknown   = 8
+    Water_Reactive_Substance = 9
+    Incompatible   = 10
+    Caution   = 11
+    Self_Reactive  = 12
 
     #dummy function for now...
     def get_type_from_id(x):
         return x
 
-print(Consequence.F)
+    def look_up_type(types, reaction_matrix):
+        results = set()
+        for t1 in types:
+            for t2 in types:
+                results.update(look_up_a_b(t1, t2, reaction_matrix))
+
+        return results
+
+
+    def look_up_a_b(a, b, reaction_matrix):
+        if a > b:
+            a, b = b, a
+
+        if a in reaction_matrix and b in reaction_matrix[a]:
+            return map(Problem.get_type_from_id, reaction_matrix[a][b])
+        else:
+            return {Problem.get_type_from_id(a), Problem.get_type_from_id(b)}
 
 
