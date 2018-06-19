46aca6ba2b6240338eceb800511b3e7e8c7c1751
lilott8 <lilott8@gmail.com>
2018-06-19 10:20:19 -0700

    getting identification working.

--- chemicals/identification/__init__.py
+++ chemicals/identification/__init__.py
@@ -1,2 +1,3 @@
 from chemicals.identification.db_identifier import DBIdentifier
 from chemicals.identification.identifier import Identifier
+from chemicals.identification.naive_identifier import NaiveIdentifier
---
+++ chemicals/identification/naive_identifier.py
@@ -0,0 +1,6 @@
+from chemicals.identification.identifier import Identifier
+
+
+class NaiveIdentifier(Identifier):
+    def __init__(self):
+        Identifier.__init__(self)
