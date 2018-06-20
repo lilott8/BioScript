7bc9ffc325c40700168d91cf49975856ba5e7a7c
Daniel Tan <danieljktan@yahoo.com>
2018-06-20 11:49:39 -0700

    database query

--- chemicals/identification/db_identifier.py
+++ chemicals/identification/db_identifier.py
@@ -1,12 +1,24 @@
 import re
 from chemicals.identification.identifier import Identifier
-
+from io.database import Database
 
 class DBIdentifier(Identifier):
     def __init__(self, db):
         Identifier.__init__(self)  
         self.db = db
 
+    def is_name(self, string):
+        self.db.sql_query('SELECT NAME things....')
 
+    def search_by_cas_number(self, string):
+        self.db.sql_query('SELECT CAS NUMBER thingy...')
 
+    def search_by_inchi_key(self, string):
+        self.db.sql_query('SELECT inchi key thingy...')
+
+    def search_by_smiles(self, string):
+        self.db.sql_query('SELECT smiles thingy...')
+
+    def search_by_aliases(self, string):
+        self.db.sql_query('SELECT aliases thingy...')
 
