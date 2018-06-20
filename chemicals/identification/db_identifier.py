41b95bf5397234d5743646007a57bdb096239a82
Daniel Tan <danieljktan@yahoo.com>
2018-06-20 11:00:41 -0700

    Regexes

--- chemicals/identification/db_identifier.py
+++ chemicals/identification/db_identifier.py
@@ -1,6 +1,12 @@
+import re
 from chemicals.identification.identifier import Identifier
 
 
 class DBIdentifier(Identifier):
     def __init__(self, db):
-        Identifier.__init__(self)
+        Identifier.__init__(self)  
+
+
+
+
+
