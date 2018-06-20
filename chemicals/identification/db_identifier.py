730ca4cb1af3eb3130f46ac9b10591987f9268f6
Daniel Tan <danieljktan@yahoo.com>
2018-06-20 11:05:15 -0700

    started db_identifier

--- chemicals/identification/db_identifier.py
+++ chemicals/identification/db_identifier.py
@@ -5,7 +5,7 @@ from chemicals.identification.identifier import Identifier
 class DBIdentifier(Identifier):
     def __init__(self, db):
         Identifier.__init__(self)  
-
+        self.db = db
 
 
 
