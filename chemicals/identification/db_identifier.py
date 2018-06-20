7fcba3ace0b5043a93adc28091dd551e2f46ab7f
Daniel Tan <danieljktan@yahoo.com>
2018-06-20 13:01:22 -0700

    Get rid of syntax errors

--- chemicals/identification/db_identifier.py
+++ chemicals/identification/db_identifier.py
@@ -6,19 +6,41 @@ class DBIdentifier(Identifier):
     def __init__(self, db):
         Identifier.__init__(self)  
         self.db = db
-
+    
+    #according to mysql syntax, '=' means check for equality....
+    #check whether name is valid
     def is_name(self, string):
-        self.db.sql_query('SELECT NAME things....')
+        cursor = self.db.sql_query('SELECT name FROM chemicals WHERE name = {0};'.format(string))
+        return len(cursor.fetchone()) != 0 #fetch a single row from cursor
 
     def search_by_cas_number(self, string):
-        self.db.sql_query('SELECT CAS NUMBER thingy...')
+        cursor = self.db.sql_query('SELECT * FROM cas_numbers WHERE cas_number_string = {0}'.format(string))
+        return cursor.fetchall()
+
 
     def search_by_inchi_key(self, string):
-        self.db.sql_query('SELECT inchi key thingy...')
+        cursor = self.db.sql_query('SELECT * FROM chemicals WHERE inchi_key = {0}'.format(string))
+        return cursor.fetchall()
 
     def search_by_smiles(self, string):
-        self.db.sql_query('SELECT smiles thingy...')
+        cursor = self.db.sql_query('SELECT * FROM chemicals WHERE isomeric_smiles = {0} OR canonical_smiles = {0}'.format(string))
+        return cursor.fetchall()
+
+
+    def search_by_pub_chem_id(self, string):
+        cursor = self.db.sql_query('SELECT * FROM chemicals WHERE pubchem_id = {0}', string)
+        return cursor.fetchall()
+
 
     def search_by_aliases(self, string):
-        self.db.sql_query('SELECT aliases thingy...')
+        cursor = self.db.sql_query('SELECT * FROM chemicals')
+        return cursor.fetchall()
+
+
+
+
+
+
+
+
 
--- chemicals/identification/identifier.py
+++ chemicals/identification/identifier.py
@@ -1,10 +1,8 @@
-
+import re
 
 class Identifier(object):
      
     cas_number_regex = re.compile('^[0-9].*-.*-.*[0-9]$')
-
-    #direct copy from the Java code.
     formula_regex    = re.compile('[A-Z][a-z]?\\d*|\\((?:[^()]*(?:\\(.*\\)0?[^()]*)+\\)\\d+')
     smiles_regex     = re.compile('OC[C@@H](O1)[C@@H](0)[C@H](0)[C@@H]2[C@@H]1c3c(O)c(OC)c(O)cc3C(=O)O2')
     inchi_key_regex   = re.compile('^([0-9A-Z\\-]+)$')
@@ -15,21 +13,21 @@ class Identifier(object):
   
     @staticmethod
     def is_cas_number(string):
-        return DBIdentifier.cas_number_regex.match(string) != None
+        return Identifier.cas_number_regex.match(string) != None
 
     @staticmethod
     def is_chemical_formula(string):
-        return DBIdentifier.formula_regex.match(string) != None
+        return Identifier.formula_regex.match(string) != None
     
     @staticmethod
     def is_smiles(string):
-        return DBIdentifier.smiles_regex.match(string) != None
+        return Identifier.smiles_regex.match(string) != None
 
     @staticmethod
     def is_inchi_key(string):
         return len(string) == 25 \
                && string[14] == '-'
-               && DBIdentifier.inchi_key_regex.match(string) != None
+               && Identifier.inchi_key_regex.match(string) != None
   
 
 
