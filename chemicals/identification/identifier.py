1879839a84c854659bd804700ed44db8a5637018
Daniel Tan <danieljktan@yahoo.com>
2018-07-16 15:56:00 -0700

    Added InChI and SMILES validation, still needs some work

--- chemicals/identification/identifier.py
+++ chemicals/identification/identifier.py
@@ -3,9 +3,9 @@ import re
 class Identifier(object):
      
     cas_number_regex = re.compile('^[0-9]{2,7}-[0-9]{2}-[0-9]$')
-    formula_regex    = re.compile('[A-Z][a-z]?\\d*|\\((?:[^()]*(?:\\(.*\\))?[^()]*)+\\)\\d+')
+    formula_regex    = re.compile('\\(?[A-Z][a-z]?\\d*\\)?')
     smiles_regex     = re.compile('OC[C@@H](O1)[C@@H](0)[C@H](0)[C@@H]2[C@@H]1c3c(O)c(OC)c(O)cc3C(=O)O2')
-    inchi_key_regex  = re.compile('^([0-9A-Z\-]+)$')
+    inchi_key_regex  = re.compile('^InChI=([0-9A-Z\-]+)$')
 
 
     def __init__(self):
@@ -25,9 +25,7 @@ class Identifier(object):
 
     @staticmethod
     def is_inchi_key(string):
-        return len(string) == 25 \
-               and string[14] == '-' \
-               and Identifier.inchi_key_regex.match(string) != None
+        return Identifier.inchi_key_regex.match(string) != None
   
 
 
--- unit_tests.py
+++ unit_tests.py
@@ -26,26 +26,62 @@ class Test_Identifiers(unittest.TestCase):
         self.assertFalse(Identifier.is_cas_number('8999-f2-11'))
         self.assertFalse(Identifier.is_cas_number('1020-0!-1'))
         self.assertFalse(Identifier.is_cas_number('2938-01o-1'))
+
         
     def test_database_connection(self):
         db = Database('root', '', 'localhost', 1433, 'chem_trails')
         self.assertTrue(db.is_connected)
         db.close()
 
+
     def test_chemical_formula_validation(self):
-        #@Incomplete: figure out what a chemical formula looks like...
-        pass
+        self.assertTrue(Identifier.is_chemical_formula('H2O'))
+        self.assertTrue(Identifier.is_chemical_formula('C2O'))
+        self.assertTrue(Identifier.is_chemical_formula('Al2(SO4)3'))
+        self.assertTrue(Identifier.is_chemical_formula('(CH3)3CH'))
+        self.assertFalse(Identifier.is_chemical_formula('3('))
+        self.assertFalse(Identifier.is_chemical_formula('(H2()'))
+        self.assertFalse(Identifier.is_chemical_formula('2HO'))
+
 
     def test_smile_validation(self): 
-        self.assertTrue(Identifier.is_smile('N#N')
-        self.assertTrue(Identifier.is_smile('CN=C=O')
-        self.assertTrue(Identifier.is_smile('[Cu+2].[O-]S(=O)(=O)[O-]')
-        self.assertTrue(Identifier.is_smile('OC[C@@H](O1)[C@@H](O)[C@H](O)[C@@H](O)[C@@H](O)1')
+        self.assertTrue(Identifier.is_smile('N#N'))
+        self.assertTrue(Identifier.is_smile('CN=C=O'))
+        self.assertTrue(Identifier.is_smile('[Cu+2].[O-]S(=O)(=O)[O-]'))
+        self.assertTrue(Identifier.is_smile('OC[C@@H](O1)[C@@H](O)[C@H](O)[C@@H](O)[C@@H](O)1'))
+        self.assertFalse(Identifier.is_smile(''))
+
+
+    def test_inchi_validation(self):
+        self.assertTrue(Identifier.is_inchi_key('InChI=1/C2H6O/c1-2-3/h3H,2H2,1H3'))
+        self.assertTrue(Identifier.is_inchi_key('InChI=1/C6H8O6/c7-1-2(8)5-3(9)4(10)6(11)12-5/h2,5,7-10H,1H2/t2-,5+/m0/s1'))
+        self.assertFalse(Identifier.is_inchi_key(''))
+        self.assertFalse(Identifier.is_inchi_key('Hello'))
 
 
 
-if __name__ == '__main__':
-    unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
