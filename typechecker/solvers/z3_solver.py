68adca775f30dd39ed7aaea1ff8d1280e03e33c5
Daniel Tan <danieljktan@yahoo.com>
2018-07-23 17:34:07 -0700

    got smt2 function working

--- typechecker/solvers/z3_solver.py
+++ typechecker/solvers/z3_solver.py
@@ -1,28 +1,37 @@
 from typechecker.solvers.base_solver import BaseSolver
 import z3
 
+#z3prover.github.io/api/html/namespacez3py.html#a09fe122cbfbc6d3fa30a79850b2a2414
 
+#parse_smt2_file, parse_smt2_string
 #z3-solver can be installed through: pip3 install z3-solver
 class Z3Solver(BaseSolver):
 
     def __init__(self):
         BaseSolver.__init__(self)
-        self.solver = z3.Solver()
+    
+    def solve_with_smt2(self, smt2_string):
+        try:
+            solver    = z3.Solver()
+            solver.add(z3.parse_smt2_string(smt2_string))
+            status    = solver.check()
+            if status == z3.CheckSatResult(z3.Z3_L_TRUE):
+                log.trace('This BioScript program is safe.')
+                return True
+            else:
+                log.error('This BioScript program may be unsafe for execution, halting compilation')
+                return False
 
-    def solve(self, problem):
-        self.solver.solve(problem)
+        except z3.Z3Exception as e:
+            log.error('There was an error solving the given constraints')
+            log.error(str(e))
+            return False
 
-    def check(self):
-        return self.solver.check()
 
-    def model(self):
-        return self.solver.model()
 
-    def create_int(self, var_name):
-        return z3.Int(var_name)
 
-    def create_real(self, var_name):
-        return z3.Real(var_name)
+
+
 
 
 
