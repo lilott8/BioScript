bdf2ddfd7785a79bb7ede7d5a4474bd57c340b3a
Daniel <danieljktan@yahoo.com>
2018-09-11 14:45:59 -0700

    experimenting around with fitting the graph coloring bin into the program

--- solvers/z3_solver.py
+++ solvers/z3_solver.py
@@ -2,13 +2,14 @@
 # z3-solver can be installed through: pip3 install z3-solver
 import z3
 
-from solvers import BaseSolver
+#from solvers import BaseSolver
 
 
-class Z3Solver(BaseSolver):
+class Z3Solver:#(BaseSolver):
 
     def __init__(self):
-        BaseSolver.__init__(self)
+        pass
+        #BaseSolver.__init__(self)
 
     def solve(self, problem) -> bool:
         # if self.config.debug:
@@ -29,3 +30,40 @@ class Z3Solver(BaseSolver):
             self.log.error('There was an error solving the given constraints')
             self.log.error(str(e))
             return False
+
+
+
+
+    def solve_graph(self, ver_weights, uses, edges):
+        """
+        Parameters:
+        ver_weights: length of weights == number of vertices in graph, the elements correspond to the weights of the nodes.
+        uses:        list of amount of colors that can be used.
+        edges:       list of tuples such that (0, 1) = edge between nodes 0 and 1 
+    
+        Output:
+        list of colors the nodes can be colored.
+        """
+        solver          = z3.Optimize()
+        v_list          = [z3.Int('x_%s' % x) for x in range(len(ver_weights))]
+        v_constraint    = [z3.And(x >= 0, x < len(ver_weights)) for x in v_list]
+        edges           = [v_list[v1] != v_list[v2] for v1, v2 in edges]
+        bin_constraint  = [z3.PbLe(tuple((ver == v_id, weight) for ver, weight in zip(v_list, ver_weights)), use) \
+                           for v_id, use in enumerate(uses)]
+    
+        solver.add(v_constraint + edges + bin_constraint)
+        solver.minimize(z3.Sum(v_list))
+        return solver.model() if solver.check() == z3.sat else None
+
+
+
+s = Z3Solver()
+model = s.solve_graph([5, 3, 2, 4], [2, 5, 4, 3], [(0, 1), (1, 3), (2, 3), (0, 2)])
+if model != None:
+    print(model)
+else:
+    print('unsat')
+
+
+
+
