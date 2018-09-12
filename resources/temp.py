757ae31adb6341eed85caa504dd39375eedf4a92
Daniel <danieljktan@yahoo.com>
2018-09-12 14:01:02 -0700

    working to get the graphing bin working given the .json file

--- resources/temp.py
+++ resources/temp.py
@@ -1,29 +1,6 @@
 import json
 
-file = open('storage.json')
-parsed = json.loads(file.read())
-
-for i, shelves in enumerate(parsed['storage']['shelves']):
-    print('SHELF %d: ' % i)
-    
-    #create graph for modeling shelf storage:
-    for chem in shelves['store']:
-        print('    ', chem)#['pubchem_id']
-
-    #figure bin storage:
-    print('     quantity: ', shelves['quantity'])#['max'], ['current']
-    print('     volume:   ', shelves['volume'])    #['max'], ['current']
-
-
-print('MANIFEST: ')
-for chem in parsed['manifest']:
-    #create graph for chemicals to be stored:
-    print('    ', chem)#['volume']['pubchem_id']
-
-
-
-
-def solve_graph(self, ver_weights, uses, edges):
+def solve_graph(ver_weights, uses, edges):
     """
     Parameters:
     ver_weights: length of weights == number of vertices in graph, the elements correspond to the weights of the nodes.
@@ -43,5 +20,36 @@ def solve_graph(self, ver_weights, uses, edges):
     solver.add(v_constraint + edges + bin_constraint)
     solver.minimize(z3.Sum(v_list))
     return solver.model() if solver.check() == z3.sat else None
+
+
+def solve_problem(file_name):
+
+    parsed = {}
+    with open(file_name) as file:
+        parsed = json.loads(file.read())
+
+    quantity = parsed['storage']['quantity']
+    print(quantity)
+    for shelves in parsed['storage']['shelves']:
+    
+        #create graph for modeling shelf storage:
+        for chem in shelves['store']:
+            print('    ', chem)#['pubchem_id']
+
+            #figure bin storage:
+            print('     quantity: ', shelves['quantity'])#['max'], ['current'], ['reactive_groups']
+            print('     volume:   ', shelves['volume'])  #['max'], ['current'], ['reactive_groups']
+
+
+    print('MANIFEST: ')
+    for chem in parsed['manifest']:
+        #create graph for chemicals to be stored:
+        print('    ', chem)#['volume']['pubchem_id']['reactive_groups']
+
+
+
+
+solve_problem('storage.json')
+
 
 
