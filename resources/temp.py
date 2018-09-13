1bb804b041e2005d5ab4186ac51ff843f8f95873
Daniel <danieljktan@yahoo.com>
2018-09-13 11:30:28 -0700

    1st draft of the problem formalized

--- resources/temp.py
+++ resources/temp.py
@@ -1,65 +1,65 @@
 import json
 import z3
 
-def solve_graph(ver_weights, uses, edges):
+def solve_problem(file_name, safe_funct = lambda x, y: False):
     """
-    Parameters:
-    ver_weights: length of weights == number of vertices in graph, the elements correspond to the weights of the nodes.
-    uses:        list of amount of colors that can be used.
-    edges:       list of tuples such that (0, 1) = edge between nodes 0 and 1 
-    
-    Output:
-    list of colors the nodes can be colored.
-    """
-    solver          = z3.Optimize()
-    v_list          = [z3.Int('x_%s' % x) for x in range(len(ver_weights))]
-    v_constraint    = [z3.And(x >= 0, x < len(ver_weights)) for x in v_list]
-    edges           = [v_list[v1] != v_list[v2] for v1, v2 in edges]
-    bin_constraint  = [z3.PbLe(tuple((ver == v_id, weight) for ver, weight in zip(v_list, ver_weights)), use) \
-                       for v_id, use in enumerate(uses)]
-    
-    solver.add(v_constraint + edges + bin_constraint)
-    solver.minimize(z3.Sum(v_list))
-    return solver.model() if solver.check() == z3.sat else None
-
-
-def solve_problem(file_name):
-    '''
     open .json file and solve the problem given in the file.
-    '''
+    """
     parsed = {}
     with open(file_name) as file:
         parsed = json.loads(file.read())
 
     quantity = parsed['storage']['quantity']
-    #^^^^^^^
-    #number of shelves in storage,
-    #this variable will be used to
-    #determine number of vertices in graph
-    #
 
-    #Perhaps this is slow, but doing it this way for readability...
     solver   = z3.Optimize()
-    
-    #store chemicals as (id, volume taken up)
-    chems_to_store       = [(chem['chemical']['pubchem_id'], chem['chemical']['volume']) \
-                            for chem in parsed['manifest'] if chem != {} ]
-    chems_binned         = [(chem['chemical']['pubchem_id'], chem['chemical']['volume']) \
-                            for shelves in parsed['storage']['shelves'] \
-                                for chem in shelves['store'] if chem != {} ]
-    chems_all            = chems_to_store + chems_binned
 
+    #chemicals to store as (id, volume)
+    chems_store_ids = [chem['chemical']['pubchem_id'] for chem in parsed['manifest'] if chem != {} ]
+    chems_store_volume = [chem['chemical']['volume'] for chem in parsed['manifest'] if chem != {} ]
+    chems_store_num = len(chems_store_ids)
+
+    #chemicals already stored as (shelf num, id, volume)
+    chems_binned_shelf_num = [num for shelves in parsed['storage']['shelves'] for num, chem in enumerate(shelves['store']) if chem != {}]
+    chems_binned_ids = [chem['chemical']['pubchem_id'] for shelves in parsed['storage']['shelves'] for chem in shelves['store'] if chem != {}]
+    chems_binned_num = len(chems_binned_ids)
     bin_volumes = [(shelves['volume']['max'] - shelves['volume']['current']) for shelves in parsed['storage']['shelves']]
+
+    #which chemicals can be safely stored together.    
+    chems_to_store_shelf_num   = [z3.Int('chem_store_%s' %x) for x in range(chems_store_num)]
+    chems_to_store_shelf_num_c = [z3.And(x >= 0, x < quantity) for x in chems_to_store_shelf_num]
     
-    chems_to_store_color = [z3.Int('mani_%s' % x) for x in range(len(chems_to_store))]
-    chems_binned_color   = [z3.Int('bin_%s' %x) for x in range(len(chems_binned))]
-    vertex_constraint    = [z3.And(v >= 0, v < quantity) for v in chems_all]
-    #edges, bin constraint
-    edges                = ...
-    bin_constraint       = ...
+    #edges:
+    for i in range(chems_store_num):
+        for j in range(i, chems_store_num):
+            if not safe_funct(chems_store_ids[i], chems_store_ids[j]):
+                solver.add(chems_to_store_shelf_num[i] != chems_to_store_shelf_num[j])
+
+    for i in range(chems_store_num):
+        for j in range(chems_binned_num):
+            if not safe_funct(chems_store_ids[i], chems_binned_ids[j]):
+                solver.add(chems_to_store_shelf_num[i] != chems_binned_shelf_num[j])
+
+    #bin constraint:
+    bin_constraint = [z3.PbLe(tuple((shelf == bin_num, vol) for shelf, vol in zip(chems_to_store_shelf_num, chems_store_volume)), volume) \
+                      for bin_num, volume in enumerate(bin_volumes)]
+    solver.add(bin_constraint)
+    solver.minimize(z3.Sum(chems_to_store_shelf_num))
+    
+    #check & solve: 
+    if solver.check() == z3.sat:
+        model = solver.model()
+        return [(num, model.evaluate(num)) for num in chems_to_store_shelf_num]
+    else:
+        return solver.model() if solver.check() == z3.sat else None
 
 
-solve_problem('storage.json')
 
+
+#chems_binned_volume = [chem['chemical']['volume'] for shelves in parsed['storage']['shelves'] for chem in shelves['store'] if chem != {}]
+m = solve_problem('storage.json', lambda x, y: x not in {1, 7, 15} and y not in {1, 7, 15})
+if m != None:
+    print(m)
+else:
+    print('unsat')
 
 
