4d2e3d4517ad30cbd1b8a2d7fd07a3f89b2c895f
lilott8 <lilott8@gmail.com>
2018-10-25 15:40:00 -0700

    getting simd implemented.

--- main.py
+++ main.py
@@ -30,8 +30,6 @@ def main(args):
         logging.info("Running a storage problem")
         problem = Storage()
 
-    raise Exception("Revisit line 89 in TargetVisitor.  You need to reconcile mixing an array with a global variable.")
-
     problem.run()
 
 
