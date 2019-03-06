b7be03a0495a5225ee816162243c53505c5e509e
lilott8 <lilott8@gmail.com>
2019-03-06 09:32:38 -0800

    fixing some collateral damage from the monster changes.

--- solvers/base_solver.py
+++ solvers/base_solver.py
@@ -1,13 +1,10 @@
 import colorlog
 
-import config
-
 
 class BaseSolver(object):
 
     def __init__(self):
         self.log = colorlog.getLogger(self.__class__.__name__)
-        self.config = config.Config.get_instance(None)
 
     def solve(self, problem) -> bool:
         raise NotImplementedError
