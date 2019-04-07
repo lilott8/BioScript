c16fc2cda1133c65550391916b3abf4a17741e83
Daniel Jun Kuan Tan <daniel@localhost.localdomain>
2019-04-06 20:18:08 -0700

    inlining functions, need to work on inlining parameters

--- compiler/targets/clang_target.py
+++ compiler/targets/clang_target.py
@@ -47,14 +47,14 @@ class ClangTarget(BaseTarget):
             #don't know what to do here
             assert(False)
         elif ChemTypes.NULL in types or ChemTypes.UNKNOWN in types:
-            #TODO: I don't know what to do here....
-            return 'void *'
+            #unknown type
+            #
+            return 'None'
         else:
             return 'mat'
 
 
-    @staticmethod
-    def construct_basic_block_code(instructions, inline=False):
+    def construct_basic_block_code(self, instructions, inline=False):
         code = ''
         for instr in instructions:
             if type(instr) == Dispose:
@@ -91,7 +91,8 @@ class ClangTarget(BaseTarget):
                 pass 
             elif type(instr) == Call:
                 if inline == True:
-                    code += '  INLINED CODE:\n'
+                    ret = ClangTarget.get_type_string(instr.function.types)
+                    code += self.inlined_code_block(instr.name, instr.defs.name)
                 else:
                     ret = ClangTarget.get_type_string(instr.function.types)
                     args = ''
@@ -109,10 +110,68 @@ class ClangTarget(BaseTarget):
         return code
 
 
+    def inlined_code_block(self, func_name, ret):
+        '''
+        alpha conversion of variables based on func_name e.g.
+        a function fibonacci with a variable named "a" will
+        have variable alpha converted to the name "fibonacci_a"
+        '''
+        func_info = self.program.symbol_table.functions[func_name]
+        code = '  {} {};\n'.format(ClangTarget.get_type_string(func_info.types), ret)
+        code += '  {\n    //inlined code\n'
+        for arg in func_info.args:
+            var_type = ClangTarget.get_type_string(arg.types)
+            code += '    {} {}_{};//TODO: pass the parameters into the function\n'.format(var_type, func_name, arg.name)
+
+        for block in self.program.functions[func_name]['blocks'].values():
+            for instr in block.instructions:
+                if type(instr) == Dispose:
+                    code += '    dispose({}_{});\n'.format(func_name, instr.uses[0].name)
+                elif type(instr) == Mix:
+                    code += '    mat {}_{} = mix({}_{}, {}, {}_{}, {}, {});\n'.format(
+                                                     func_name, instr.defs.name,
+                                                     func_name, instr.uses[0].name,
+                                                     instr.uses[0].size,
+                                                     func_name, instr.uses[1].name,
+                                                     instr.uses[1].size,
+                                                     1000)
+                elif type(instr) == Split:
+                    code += '    mat {}_{} = split({}_{}, {});\n'.format(
+                                             func_name, instr.defs.name,
+                                             func_name, instr.uses[0].name,
+                                             instr.uses[0].size)
+                elif type(instr) == Detect:
+                    code += '    double {}_{} = detect({}, {}_{}, {});\n'.format(
+                                             func_name, instr.defs.name,
+                                             instr.module.name,
+                                             func_name, instr.uses[0].name,
+                                             instr.module.size)
+                elif type(instr) == Heat:
+                    code += '    mat {}_{} = heat({}_{}, {}, {});\n'.format(
+                                             func_name, instr.defs.name,
+                                             func_name, instr.uses[0].name,
+                                             instr.uses[0].size, instr.uses[0].size) 
+                elif type(instr) == Dispense:
+                    code += '    mat {}_{} = dispense({}_{}, {});\n'.format(
+                                             func_name, instr.defs.name,
+                                             func_name, instr.uses[0].name, instr.uses[0].size)
+                elif type(instr) == Return:
+                    if type(instr.return_value) == Chemical or type(instr.return_value) == RenamedVar:
+                        code += '    {} = {}_{};\n'.format(
+                                             ret, func_name, instr.return_value.name)
+                    elif type(instr.return_value) == Number:
+                        code += '    {} = {}_{};\n'.format(
+                                             ret, func_name, instr.return_value.name) 
+                elif type(instr) == Call:
+                    #nested inlined functions currently not supported!!!
+                    assert(False)
+        code += '  }\n' 
+        return code
+
 
     def transform(self):
         #TODO: fix when inlining is truly implemented
-        INLINE = False 
+        INLINE = True 
 
         #a list of strings that represents all the function code
         self.function_code = []
@@ -167,7 +226,7 @@ class ClangTarget(BaseTarget):
         if INLINE == True:
             self.compiled += 'int main(int argc, char const **argv) {\n'
             for block in self.program.functions['main']['blocks'].values():
-                self.compiled += ClangTarget.construct_basic_block_code(block.instructions, inline=INLINE)
+                self.compiled += self.construct_basic_block_code(block.instructions, inline=INLINE)
             self.compiled += '}\n\n'
         else:
             code_func = []
@@ -179,10 +238,19 @@ class ClangTarget(BaseTarget):
                     func_info = self.program.symbol_table.functions[func_name]
                     ret = ClangTarget.get_type_string(func_info.types)
                     args = ''
+
+                    for arg in func_info.args:
+                        var_name = arg.name
+                        var_type = ClangTarget.get_type_string(arg.types)
+                        if args:
+                            args += ', {} {}'.format(var_type, var_name)
+                        else:
+                            args = '{} {}'.format(var_type, var_name)
+
                     self.compiled += '{} {}({});\n\n'.format(ret, func_name, args)
                     code = '{} {}({}) '.format(ret, func_name, args) + '{\n'
                 for block in function['blocks'].values(): 
-                    code += ClangTarget.construct_basic_block_code(block.instructions, inline=INLINE)
+                    code += self.construct_basic_block_code(block.instructions, inline=INLINE)
                 code += '}\n\n'
                 code_func.append(code)
  
