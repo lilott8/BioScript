eb6e9f03c191930c1f8e577a45bdc920036bba66
startwarfields <jmeye014@ucr.edu>
2019-07-01 16:14:14 -0700

    Planning Steps for Upgrading Identify stage to correctly identify all loops, not just cycles

--- compiler/passes/transforms/loop_unroll.py
+++ compiler/passes/transforms/loop_unroll.py
@@ -51,16 +51,35 @@ class LoopUnroll(BSTransform):
         else: i_step = True
 
         return True if zk_delta > 0 and i_step else False
+    def give_graph_loop(self,tlist):
+        # Algorithm Steps
+        # Let s be a set of nodes including header node h
+        # 1) for every node in s there is a path to h
+        # 2) from h there is a path to every node in s
+        # 3) there is no edge from any node outside S to any node in S other than h
+        
+        l_list = []
+        return l_list
 
     def unroll(self, program: Program) -> Program:
         global jump
         for root in program.functions:
+
             tlist = list(nx.simple_cycles(program.functions[root]['graph']))
+            node_one = list(nx.nodes(program.functions[root]['graph']))
+            # Step 1: Get cycle
+            # Step 2: Analyze Cycle
+            # This helps us verify condition 3: No outsinde inc edges
+            glist = program.functions[root]['graph'].in_edges(node_one[1])
+
+            self.log.warn(glist)
+            self.log.warn(tlist)
             for item in tlist:
                 if len(item) > 2:
                     continue
                 child = item[1]
                 parent = item[0]
+
                 pure_child_ins = copy.deepcopy(program.functions[root]['blocks'][child].instructions)
                 pure_parent_ins = copy.deepcopy(program.functions[root]['blocks'][parent].instructions)
                 c_label = program.functions[root]['blocks'][child].label
@@ -69,7 +88,7 @@ class LoopUnroll(BSTransform):
                 binary_operation = None
                 jump = None
                 labels = []
-                # IDENTIFY
+                # IDENTIFY - rewrite to implement dominator algorithm
 
                 # Parent Instructions
                 l_left = l_right = None
@@ -111,7 +130,8 @@ class LoopUnroll(BSTransform):
                 # Warning 1: This Code is not fully functional: it cannot find the original x value
                 is_finite = False
                 if bad_loop is False:
-                    constant = 1
+                    constant = 1  # Can't get current variable value, assume 1 because we already have 1 "set"
+                                    # of instructions
                     base_instructions = program.functions[root]['blocks'][child].instructions.copy()
                     is_finite = self.finite_loop(binary_operation, constant, int(binary_operation.right),
                                              label.right.value)
