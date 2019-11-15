a0a9cc2b5637ad3941b26bdb9733bc370ed1480d
vastopol <classicunix@gmail.com>
2019-11-15 15:02:03 -0800

    partial implement of the slice

--- compiler/passes/analyses/slicer.py
+++ compiler/passes/analyses/slicer.py
@@ -1,11 +1,13 @@
 from colorlog import logging
 
 from compiler.data_structures import Program
+from compiler.data_structures import IRInstruction
 from compiler.passes.analyses.bs_analysis import BSAnalysis
 
-import networkx as nx
 import json
+import networkx as nx
 import matplotlib.pyplot as plt
+from collections import defaultdict
 from itertools import count
 import colorlog.logging
 
@@ -21,11 +23,10 @@ except ImportError:
         raise ImportError("This example needs Graphviz and either "
                           "PyGraphviz or pydot")
 
-from collections import defaultdict
+
 #TODO: ENSURE YOU HAVE THE ABOVE PACKAGES INSTALLED
 # install graphviz package (apt-get) THEN graphviz(pip) or
 # Install python-graphviz (That should work)
-
 
 # Use-def chain
 # https://en.wikipedia.org/wiki/Use-define_chain
@@ -36,13 +37,6 @@ from collections import defaultdict
 # NetworkX goes away at this stage
 # Be aware of certain flow displays of mfsim
 # Goals: Build def-use chain, implicitly building the needed DAGs, Dependency Graph
-
-
-class Chain(object):
-
-    def __init__(self, block: int, instruction: int):
-        self.block = -1
-        self.instruction = -1
 
 
 class Slicer(BSAnalysis):
@@ -66,7 +60,6 @@ class Slicer(BSAnalysis):
         used = defaultdict(list)
         many_use = defaultdict(list)
         use_json = dict()
-        conds = dict()
 
         for root in program.functions:
             self.log.info("Function: {}".format(root))
@@ -80,7 +73,7 @@ class Slicer(BSAnalysis):
                     self.log.info("\t\t{}".format(instruction))
 
                     # DEF: DISPENSE, ...
-                    if str(type(instruction)) == "<class 'compiler.data_structures.ir.Dispense'>":
+                    if instruction.op == IRInstruction.DISPENSE:
                         ins = str(instruction).split('=')
                         var = ins[0].strip()
                         exp = ins[1:]
@@ -89,10 +82,7 @@ class Slicer(BSAnalysis):
                         defs[var] = (nid, i_counter)
 
                     # USE && DEF: MIX, HEAT, SPLIT, ...
-                    if (str(type(instruction)) == "<class 'compiler.data_structures.ir.Mix'>" or
-                            str(type(instruction)) == "<class 'compiler.data_structures.ir.Heat'>" or
-                            str(type(instruction)) == "<class 'compiler.data_structures.ir.Split'>"):
-
+                    if instruction.op == IRInstruction.MIX:
                         ins = str(instruction).split('=')
                         var = ins[0].strip()
                         exp = ins[1:]
@@ -102,21 +92,10 @@ class Slicer(BSAnalysis):
                         defs[var] = (nid, i_counter)
                         uses.extend([(parts[0].strip(), (nid, i_counter)), (parts[1].strip(), (nid, i_counter))])
 
-                    # CONTROL FLOW: CONDITIONAL
-                    if str(type(instruction)) == "<class 'compiler.data_structures.ir.Conditional'>":
-                        tmp = str(instruction).split('\t')
-                        data = (tmp[2].strip()[3:], tmp[3].strip()[3:])
-                        conds[(nid, i_counter)] = data
-
-                        # maybe recurse, or save where the branches are...
-                        # maybe if was a graph then could see where the join edges go
-                        # if 2 edges goto same node and both use a variable, mark the variable as used
-                        # if encountered again, then add
-
                     # Line Counter
                     i_counter += 1
-                    ################### Graph Code ##############################
 
+                    ################### Graph Code ##############################
                     pg = program.functions[root]['graph']
                     # for n in pg:
                         # Attribute
@@ -124,7 +103,6 @@ class Slicer(BSAnalysis):
                         # The actual name 'color' is not needed, we're just getting a numerical
 
                         # n['color'] = 0
-
 
                     # This does the structural stuff to convert all the values to a colorbar
 
@@ -163,28 +141,57 @@ class Slicer(BSAnalysis):
                                         with_labels=False, node_size=10, cmap=plt.cm.jet)
             plt.colorbar(nc)
             plt.axis('off')
-            plt.show()
+            # plt.show()
             # This might be helpful if you need higher quality, also available in pdf
-            # plt.savefig('slice.png')
+            plt.savefig('slice.png')
             ##############################################################
+
         # find the possible conflicts
         for u in uses:
             used[u[0]].append(u[1])
         for u in used:
-            # TODO: figure out with conditionals
+            # TODO: figure out with conditionals/loops
             # check if multi use occurred one after the other,
             # but not on the same level like in an if/else
             if len(used[u]) > 1:
-                self.log.warn("\n{}, {}".format(conds, used[u]))
+                blks = list()
+                for x in used[u]:
+                    blks.append(x[0])
                 many_use[u] = used[u]  # adds all uses, but usually need the last ones
 
-        # print statements with the log format info = green, warn = yellow, error = red
-        self.log.info("\nDeps: \n{} \nDefs: \n{} \nUses: \n{} \nUsed:  \n{} \nMulti-Used: \n{}"
-                      .format(deps, defs, uses, dict(used), dict(many_use)))
+        # take the union of the extra uses on each path
+        # if 2 edges goto same node and both use a variable, mark the variable as used
+        # if encountered again, then add
+        mini_use = defaultdict(set)
+        for root in program.functions:
+            pg = program.functions[root]['graph']
+            start = list(pg.nodes())[0]
+            finish = list(pg.nodes())[-1]
+            paths = list(nx.all_simple_paths(pg, start, finish))
+            if paths == list():  # special case for straight line programs
+                paths = [[1]]
+            for var in many_use:             # each func
+                for p in paths:              # each path
+                    first_u = False
+                    for x in many_use[var]:  # each use
+                        if x[0] in p and first_u:
+                            mini_use[var].add(x)
+                        if x[0] in p and not first_u:
+                            first_u = True
 
-        # output data file
-        for u in many_use:
-            use_json[u] = "{}".format(many_use[u])
+        # var : (instructions, position)
+        total = dict()
+        for m in mini_use:
+            remake = [d.strip() for d in deps[m]]
+            total[m] = (remake, mini_use[m])
+
+        # print statements with the log format info = green, warn = yellow, error = red
+        # self.log.info("\nDeps: \n{} \nDefs: \n{} \nUses: \n{} \nUsed:  \n{} \nMulti-Used: \n{}\nMini-Used: \n{}\nFinal: \n{}"
+        #               .format(deps, defs, uses, dict(used), dict(many_use), dict(mini_use), total))
+
+        # output JSON data file
+        for t in total:
+            use_json[t] = "{}".format(total[t])
         with open('data.txt', 'w') as outfile:
             json.dump(use_json, outfile)
 
