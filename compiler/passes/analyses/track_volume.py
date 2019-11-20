ce28ec5737b09ee7ed63dbc5b66ca1cabc664755
Aaron Sigal <asigal55@gmail.com>
2019-11-19 17:20:29 -0800

    Added some primitive logic for handling instructions

--- compiler/passes/analyses/track_volume.py
+++ compiler/passes/analyses/track_volume.py
@@ -2,6 +2,7 @@ import networkx as nx
 
 from compiler.data_structures import Program
 from compiler.passes.analyses.bs_analysis import BSAnalysis
+from compiler.data_structures.ir import *
 
 
 class VolumeTracker(BSAnalysis):
@@ -14,28 +15,50 @@ class VolumeTracker(BSAnalysis):
         variable_volume = dict();                # The dict that will store the volumes of relevent variables
 
         for root in program.functions:
-            for nid, block in program.functions[root]['blocks'].items():
-                print(nid)
-                print(block.instructions)
-                print("Defs: " + str(block.defs))
-                print("Uses: " + str(block.uses))
 
-        print("Symbol Table:")
-        print("globals: ")
-        print(program.symbol_table.globals)
+            print("Functions from graph: ")
+            for node, data in program.bb_graph.nodes(data=True):
+                print(program.functions[root]['blocks'][node])
 
-        print("functions: ")
-        print(program.symbol_table.functions)
+                for i in program.functions[root]['blocks'][node].instructions:
+                    print(type(i))
+                    #print(i)
+                    print(i.defs)
+                    handle(self, i)
 
-        print("scopes: ")
-        print(program.symbol_table.scope_map)
-
-        print("locals: ")
-        for scope in program.symbol_table.scope_map:
-            print(program.symbol_table.scope_map[scope])
-
-            for var in program.symbol_table.scope_map[scope].locals:
-                print(type(program.symbol_table.scope_map[scope].locals[var]))
-                print(program.symbol_table.scope_map[scope].locals[var])
 
         return {'name': self.name, 'result': None} # Not sure what is going on here, but its needed to prevent a crash
+
+
+def handle(self, instruction : IR): # The meat of the logic. Just ferries out the functions based on the type of instruction.
+    if (type(instruction) == Dispense):
+        handle_dispense(self, instruction.defs)
+        return
+
+    if (type(instruction) == Dispose):
+        handle_dispose(self, instruction.defs)
+        return
+
+    if (type(instruction) == Mix):
+        handle_mix(self, instruction.defs)
+        return
+
+    if (type(instruction) == Split):
+        handle_split(self, instruction.defs)
+        return
+
+
+
+
+
+def handle_dispense(self, instructions : dict):
+    print("Handling dispense...")
+
+def handle_dispose(self, instructions : dict):
+    print("Handling dispose...")
+
+def handle_mix(self, instructions : dict):
+    print("Handling mix...")
+
+def handle_split(self, instructions : dict):
+    print("Handling split...")
