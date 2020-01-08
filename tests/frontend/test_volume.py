4d1c48aaeb478b052fab79a520ee210ecbafa36e
Aaron Sigal <asigal55@gmail.com>
2020-01-07 19:51:19 -0800

    Added several test cases

--- tests/frontend/test_volume.py
+++ tests/frontend/test_volume.py
@@ -5,6 +5,56 @@ from compiler.data_structures.symbol_table import SymbolTable
 from shared.bs_exceptions import UndefinedVariable, UndefinedFunction, UnsupportedOperation
 from tests.frontend.front_end_base import FrontEndBase
 
+@pytest.mark.frontend
+@pytest.mark.volume
+@pytest.mark.dispose
+class TestMix(FrontEndBase):
+
+    def test_basic(self, get_visitor):
+        file = "test_cases/volume/mix_basic.bs"
+
+        tree = get_visitor(file)
+
+        vol = self.get_volume(tree, file)
+
+        assert vol[0] == False
+
+        assert sum(vol[1]['a1']['volumes']) == -1
+        assert sum(vol[1]['b1']['volumes']) == -1
+        assert sum(vol[1]['c1']['volumes']) == 20
+
+    def test_offset(self, get_visitor):
+        file = "test_cases/volume/mix_single_offset_use.bs"
+        tree = get_visitor(file)
+
+        vol = self.get_volume(tree, file)
+
+        assert vol[0] == False
+        assert sum(vol[1]['a1']['volumes']) == -1
+        assert sum(vol[1]['a_21']['volumes']) == -1
+        assert sum(vol[1]['a_31']['volumes']) == -1
+        assert vol[1]['a_s1']['volumes'][0] == -1
+        assert vol[1]['a_s1']['volumes'][1] == 10
+        assert sum(vol[1]['b1']['volumes']) == -1
+        assert sum(vol[1]['c1']['volumes']) == 20
+
+
+@pytest.mark.frontend
+@pytest.mark.volume
+@pytest.mark.dispose
+class TestDispose(FrontEndBase):
+
+    def test_basic(self, get_visitor):
+        file = "test_cases/volume/dispose.bs"
+
+        tree = get_visitor(file)
+
+        vol = self.get_volume(tree, file)
+
+        assert vol[0] == False
+        assert sum(vol[1]['a1']['volumes']) == -1
+
+
 
 @pytest.mark.frontend
 @pytest.mark.volume
@@ -34,19 +84,3 @@ class TestDispense(FrontEndBase):
 
         for i in range(5):
             assert vol[1]['b1']['volumes'][i] == 2
-
-
-@pytest.mark.frontend
-@pytest.mark.volume
-@pytest.mark.dispose
-class TestDispose(FrontEndBase):
-
-    def test_basic(self, get_visitor):
-        file = "test_cases/volume/dispose.bs"
-
-        tree = get_visitor(file)
-
-        vol = self.get_volume(tree, file)
-
-        assert vol[0] == False
-        assert sum(vol[1]['a1']['volumes']) == -1
