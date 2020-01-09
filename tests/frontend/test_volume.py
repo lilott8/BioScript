9ff0ed8020616ed09e98839f6513a6a86f798696
Aaron Sigal <asigal55@gmail.com>
2020-01-08 17:32:27 -0800

    Adjusted and implemented test cases

--- tests/frontend/test_volume.py
+++ tests/frontend/test_volume.py
@@ -5,39 +5,6 @@ from compiler.data_structures.symbol_table import SymbolTable
 from shared.bs_exceptions import UndefinedVariable, UndefinedFunction, UnsupportedOperation
 from tests.frontend.front_end_base import FrontEndBase
 
-@pytest.mark.frontend
-@pytest.mark.volume
-@pytest.mark.dispose
-class TestMix(FrontEndBase):
-
-    def test_basic(self, get_visitor):
-        file = "test_cases/volume/mix_basic.bs"
-
-        tree = get_visitor(file)
-
-        vol = self.get_volume(tree, file)
-
-        assert vol[0] == False
-
-        assert sum(vol[1]['a1']['volumes']) == -1
-        assert sum(vol[1]['b1']['volumes']) == -1
-        assert sum(vol[1]['c1']['volumes']) == 20
-
-    def test_offset(self, get_visitor):
-        file = "test_cases/volume/mix_single_offset_use.bs"
-        tree = get_visitor(file)
-
-        vol = self.get_volume(tree, file)
-
-        assert vol[0] == False
-        assert sum(vol[1]['a1']['volumes']) == -1
-        assert sum(vol[1]['a_21']['volumes']) == -1
-        assert sum(vol[1]['a_31']['volumes']) == -1
-        assert vol[1]['a_s1']['volumes'][0] == -1
-        assert vol[1]['a_s1']['volumes'][1] == 10
-        assert sum(vol[1]['b1']['volumes']) == -1
-        assert sum(vol[1]['c1']['volumes']) == 20
-
 
 @pytest.mark.frontend
 @pytest.mark.volume
@@ -53,7 +20,6 @@ class TestDispose(FrontEndBase):
 
         assert vol[0] == False
         assert sum(vol[1]['a1']['volumes']) == -1
-
 
 
 @pytest.mark.frontend
@@ -84,3 +50,88 @@ class TestDispense(FrontEndBase):
 
         for i in range(5):
             assert vol[1]['b1']['volumes'][i] == 2
+
+
+@pytest.mark.frontend
+@pytest.mark.volume
+@pytest.mark.mix
+class TestMix(FrontEndBase):
+
+    def test_basic(self, get_visitor):
+        file = "test_cases/volume/mix_basic.bs"
+
+        tree = get_visitor(file)
+
+        vol = self.get_volume(tree, file)
+
+        assert vol[0] == False
+
+        assert sum(vol[1]['a1']['volumes']) == -1
+        assert sum(vol[1]['b1']['volumes']) == -1
+        assert sum(vol[1]['c1']['volumes']) == 20
+
+    def test_offset(self, get_visitor):
+        file = "test_cases/volume/mix_single_offset_use.bs"
+        tree = get_visitor(file)
+
+        vol = self.get_volume(tree, file)
+
+        assert vol[0] == False
+        assert sum(vol[1]['a1']['volumes']) == -1
+        assert sum(vol[1]['a_21']['volumes']) == -1
+        assert sum(vol[1]['a_31']['volumes']) == -1
+        assert vol[1]['a_s1']['volumes'][0] == -1
+        assert vol[1]['a_s1']['volumes'][1] == 10
+        assert sum(vol[1]['b1']['volumes']) == -1
+        assert sum(vol[1]['c1']['volumes']) == 20
+
+    def test_double_var_use(self, get_visitor):
+        file = "test_cases/volume/mix_var_double_use.bs"
+        tree = get_visitor(file)
+
+        vol = self.get_volume(tree, file)
+
+        assert vol[0] == True
+
+
+class TestSplit(FrontEndBase):
+
+    def test_basic(self, get_visitor):
+        file = "test_cases/volume/split_basic.bs"
+
+        tree = get_visitor(file)
+
+        vol = self.get_volume(tree, file)
+
+        assert vol[0] == False
+        assert sum(vol[1]['a1']['volumes']) == -1
+        assert sum(vol[1]['b1']['volumes']) == 10
+
+        for i in range(5):
+            assert vol[1]['b1']['volumes'][i] == 2
+
+    def test_double_use(self, get_visitor):
+        file = "test_cases/volume/split_double_use.bs"
+
+        tree = get_visitor(file)
+
+        vol = self.get_volume(tree, file)
+
+        assert vol[0] == True
+
+        assert sum(vol[1]['a1']['volumes']) == -1
+
+    def test_good_num(self, get_visitor):
+        file = "test_cases/volume/split_good_num.bs"
+
+        tree = get_visitor(file)
+
+        vol = self.get_volume(tree, file)
+
+        assert vol[0] == False
+
+        assert sum(vol[1]['a1']['volumes']) == -1
+        assert sum(vol[1]['b1']['volumes']) == 30
+        assert vol[1]['b1']['volumes'][0] == 10
+        assert vol[1]['b1']['volumes'][1] == 10
+        assert vol[1]['b1']['volumes'][2] == 10
