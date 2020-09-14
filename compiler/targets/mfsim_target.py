e93fbb4c80050ad17dbc3db7647b3ce2309def40
Tyson <tlove004@ucr.edu>
2020-09-14 11:53:14 -0700

    Verified fix for finding dependent instructions. Syntax update on diazepam.bs MFSim transfer node ids now start from the next 100 after the last nodeid, including necessary node insertions for split trees. TODO added for translating while statements properly.

--- compiler/targets/mfsim_target.py
+++ compiler/targets/mfsim_target.py
@@ -9,6 +9,7 @@ from compiler.targets.base_target import BaseTarget
 from compiler.data_structures.ir import TempConstraint
 from compiler.data_structures.ir import TimeConstraint
 from compiler.data_structures.ir import UseIn
+import math
 
 
 class TransferNode:
@@ -50,13 +51,16 @@ class MFSimTarget(BaseTarget):
         self.usein_data = {"constraints": self.constraints}
         self.usein_subdata = {"useins": self.useins}
 
-        # start transfer nodes from id 100.
-        if self.config.debug:
-            self.log.debug("Statically starting transfer IDs from 100. This could be an issue for very large assays.")
         if not self.config.output:
             self.log.fatal("MFSim target requires an output path to be specified.  Include \"-o <path/to/output/>\" in arguments list.")
             exit(-1)
-        self.tid = 100
+        splits = 0
+        for fun in program.functions.values():
+            for bb in fun['blocks'].values():
+                for i in bb.instructions:
+                    if i.op is IRInstruction.SPLIT:
+                        splits += i.split_size
+        self.tid = int(math.ceil((self.program.functions['main']['blocks'][self.program.entry_point].instructions[0].id_counter + 1 + splits) / 100.0)) * 100
         self.num_dags = 0
         self.num_cgs = 0
         self.num_edges = 0
@@ -705,7 +709,12 @@ class MFSimTarget(BaseTarget):
             if cond.left['name'].startswith('REPEAT'):
                 _ret += "RUN_COUNT, LT, DAG%s, %s)\n" % (str(from_dag), int(cond.left['var'].value.value[0]))
             else:  # must be a while
-                _ret += "while)\n"  # this is the simple while setup currently, can be updated with more information
+                # TODO translate while conditions properly -- if a while is statically known
+                #    we should translate to a repeat, i.e., i = 0; while (i < 10) { ...i++...} -> repeat 10 times
+                #    otherwise, i.e.: x = detect.., while (x...) { x = detect; }, we'll need to set up the sensor
+                #    reading as the conditional
+                self.log.error("Not translating \"while\" loop in mfsim properly")
+                _ret += "while)\n"
         elif cond_type is 'IF':
             # ONE_SENSOR, relop, depDag, depNodeID, value)
             relop = "Unrecognized relational operator"
